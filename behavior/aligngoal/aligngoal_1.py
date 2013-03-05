4d120c5475d5c5958e8aa75919294f8979f35455
Rick van der Mark <R.W.van.der.Mark.1@student.rug.nl>
2013-03-05 22:43:15 +0100

    Goal aligning
    Geen flauw idee of het werkt (lees: niet crasht), vooral even een check
    of ik Git goed kan gebruiken at home.

--- behavior/aligngoal/aligngoal_1.py
+++ behavior/aligngoal/aligngoal_1.py
@@ -16,10 +16,53 @@ class AlignGoal_x(basebehavior.behaviorimplementation.BehaviorImplementation):
         self.__start_time = time.time()
 
         self.__nao = self.body.nao(0)
-        self.__nao.say("Align!")
-        
+        self.__nao.say("Aligning with goal!")
+        self.__state = "FIND"
+		self.__checked = False
+		
         self.__nao.look_forward()
-        #self.__nao.walkNav(0.15, 0.12, -(90 * almath.TO_RAD))
+		
+		# With a yellow goal:
+		while self.__checked is False:
+			if (self.m.n_occurs("combined_yellow") > 0):
+				(recogtime, obs) = self.m.get_last_observation("combined_yellow")
+				if not obs == None and recogtime > self.__last_ball_recogtime:
+					contours = obs["sorted_contours"]
+					biggest_blob = contours[0]
+					print "%s: x=%d, y=%d, width=%d, height=%d, surface=%d" \
+							% ("yellow", biggest_blob['x'], biggest_blob['y'], biggest_blob['width'], biggest_blob['height'], biggest_blob['surface'])
+					self.__last_recogtime = recogtime
+					
+					# Goal is found if the detected blob is big enough (thus filtering noise)
+					if biggest_blob['width'] < 80 and biggest_blob['height'] > 100:
+						print "Goal Detected"
+						if self.__state == "FIND":
+							self.__nao.say("In front of the goal")						
+						elif self.__state == "FIND_RIGHT":
+							self.__nao.say("Possibly turning to the goal")
+							self.__nao.walkNav(0.15,0.12,-((45 * almath.TO_RAD)+((biggest_blob['x']-80)*(-0.005))))
+							self.__nao.look_forward()
+						elif self.__state == "FIND_LEFT":
+							self.__nao.say("Possibly turning to the goal")
+							self.__nao.walkNav(0.15,0.12,((45 * almath.TO_RAD)+((biggest_blob['x']-80)*(-0.005))))
+							self.__nao.look_forward()
+			else:
+				if self.__state == "FIND":
+					self.__nao.look_right()
+					self.__state = "FIND_RIGHT"
+					self.__nao.say("Looking Right")
+				elif self.__state == "FIND_RIGHT":
+					self.__nao.look_forward()
+					self.__nao.look_left()
+					self.__state = "FIND_LEFT"
+					self.__nao.say("Looking left")
+				elif self.__state == "FIND_LEFT":
+					# Dit moet nog beter, en waarschijnlijk op een andere plek:
+					self.__nao.say("Maybe the goal is on the other side?")
+					self.__checked = True
+					self.__nao.walkNav(0.3,0.2,(180 * almath.TO_RAD))
+					self.__nao.look_forward()	
+
 
     def implementation_update(self):
         if self.idling:
@@ -27,8 +70,8 @@ class AlignGoal_x(basebehavior.behaviorimplementation.BehaviorImplementation):
         
         #TODO: Remove the following with the steps mentioned below (e.d. align the robot so that it can kick the ball in the direction of the goal):
         #It now simply assumes that it is already aligned:
-        if not self.__nao.isWalking(): 
-            if (time.time() - self.__start_time) > 10:
+        if not self.__nao.isMoving(): 
+            if (time.time() - self.__start_time) > 20:
                 self.m.add_item('subsume_stopped',time.time(),{'reason':'Ball no longer seen.'})
                 self.idling = True
             self.m.add_item('goal_aligned',time.time(),{})    
