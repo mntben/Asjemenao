1a6acbb40a76f9f7f2f12036d162e2122cbae8be
rvdmark <R.W.van.der.Mark.1@student.rug.nl>
2013-03-06 09:19:39 +0100

    Tmp files, etc

---
+++ behavior/testsubsumehigher/testsubsumehigher_x.py
@@ -0,0 +1,31 @@
+
+
+'''
+this is an automatically generated template, if you don't rename it, it will be overwritten!
+'''
+
+import basebehavior.behaviorimplementation
+
+
+class TestSubsumeHigher_x(basebehavior.behaviorimplementation.BehaviorImplementation):
+
+    '''this is a behavior implementation template'''
+
+    #this implementation should not define an __init__ !!!
+
+
+    def implementation_init(self):
+
+        #define list of sub-behavior here
+        pass
+
+    def implementation_update(self):
+
+        #you can do things here that are low-level, not consisting of other behaviors
+
+        #in this function you can check what behaviors have failed or finished
+        #and do possibly other things when something has failed
+        pass
+
+
+
