49c4b1e443a73008f4a29bfc42297995d20e1b12
asjemenao <diederikvkrieken@gmail.com>
2013-03-20 12:34:01 +0100

    Untested goal aligning based on the approachball-behavior. Nao walks backwards with an angle (depending on the headangle) and realigns with the ball.

--- behavior/aligngoal/aligngoal_0.py
+++ behavior/aligngoal/aligngoal_0.py
@@ -74,11 +74,36 @@ class AlignGoal_x(basebehavior.behaviorimplementation.BehaviorImplementation):
                     print "Big goal"
                     self.idling = True
                     if self.__state == "F2":
-                        self.__nao.walkNav(0.15,0.15,headAngle)
+                        self.__nao.walkNav(-0.4,0.2,headAngle, 0.01)
                     elif self.__state == "F3":
-                        self.__nao.walkNav(0.15,-(0.15),headAngle)
-                    self.m.add_item('goal_aligned',time.time(),{})
-                    return
+                        self.__nao.walkNav(-0.4,-0.2,headAngle, 0.01)
+                    self.__nao.look_forward_down()
+                    if (self.m.n_occurs("combined_red") > 0):
+                        (recogtime, obs) = self.m.get_last_observation("combined_red")
+                        if not obs == None and recogtime > self.__last_ball_recogtime:
+                            self.__last_ball_recogtime = recogtime
+                            contours = obs["sorted_contours"]
+                            biggest_blob = contours[0]
+                            if biggest_blob['surface'] > 30 and biggest_blob['surface'] < 950:
+                                self.__ball_last_seen = time.time()
+                                blob_center_x = biggest_blob['x'] + biggest_blob['width']/2
+                                blob_center_y = biggest_blob['y'] + biggest_blob['height']/2
+                                if blob_center_y > 90 and blob_center_x > 30 and blob_center_x < 130 and not self.__is_looking_horizontal:
+                                    self.__nao.stopwalk()
+                                    self.m.add_item('goal_aligned',time.time(),{})
+                                    print "Aligned with goal!"
+                                    return
+                                if blob_center_y < 90:
+                                    if self.__is_looking_horizontal:
+                                        X = (((((blob_center_y -90)/90)**2)*0.25)+0.75)
+                                    else:
+                                        X = (((((blob_center_y -90)/90)**2)*0.75)+0.25)
+                                    
+                                    Theta = math.copysign(((((blob_center_x -80)/80)**(2))/2), ((blob_center_x -80)*-1))
+                                    #Value found after enourmous amounts of experiments by trial and error
+                                    self.__nao.moveToward(X, 0, Theta)
+                                    print "blob_center_Y=%d    Walk_X=%f,  blob_center_x=%d  Walk_Theta=%f" % (blob_center_y, X, blob_center_x, Theta)
+                                    pass
         elif not biggest_target == None and not biggest_own == None and recogtime_target > ( time.time() - 5 ):
             print "Two markers"
             if biggest_target['surface'] > 50 and biggest_target['surface'] < 2000:
